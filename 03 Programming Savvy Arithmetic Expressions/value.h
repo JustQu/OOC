667576adb705768f2a1b40be8b8d7c799b32e13b
JustQu <dmelessa@JustQu.localdomain>
2019-09-29 20:19:55 +0300

    oops

--- 03 Programming Savvy Arithmetic Expressions/value.h
+++ 03 Programming Savvy Arithmetic Expressions/value.h
@@ -1,15 +1,15 @@
-#ifndef VALUE_H
-# define VALUE_H
+#ifndef	VALUE_H
+#define	VALUE_H
 
-const void	*Minus;
-const void	*Value;
-const void	*Add;
-const void	*Sub;
-const void	*Mult;
-const void	*Div;
+const void * Minus;
+const void * Value;
+const void * Mult;
+const void * Div;
+const void * Add;
+const void * Sub;
 
-void	*new(const void	*type, ...);
-void	process(const void *tree);
-void	delete(void	*tree);
+void * new (const void * type, ...);
+void process (const void * tree);
+void delete (void * tree);
 
 #endif
